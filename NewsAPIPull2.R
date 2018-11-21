462b2bb7b576d7e2ef91f57cf510b9ba78a9cb7a
nick3703 <nick3703@hotmail.com>
2018-11-21 10:03:39 -0500

    News API Push

--- NewsAPIPull2.R
+++ NewsAPIPull2.R
@@ -9,10 +9,10 @@ library(RSelenium)
 library(XML)
 library(rvest)
 #Enter Search Term 
-term<-'"West+Point"'
+term<-"USMA"  #Include Branch Night
 key<-apikey
 
-USMA.JSON<-fromJSON(paste0("https://newsapi.org/v2/everything?q=",term,"&apiKey=",key,sep=""))
+USMA.JSON<-fromJSON(paste0("https://newsapi.org/v2/everything?q=",term,"&from=2018-11-10&apiKey=",key,sep=""))
 article.df<-USMA.JSON$articles
 article.df$source<-article.df$source$name
 #Classify sentiment of Articles
@@ -20,7 +20,7 @@ sent <- sentiment_by(article.df$content
 )
 article.df$sentiment <- sent$ave_sentiment * 100
 
-neg.art<-article.df %>% filter(sentiment< -20)
+neg.art<-article.df %>% filter(sentiment< -10)
 
 pos.art<-article.df %>% filter(sentiment >10) 
 
@@ -32,9 +32,9 @@ neg.art$source
 neg.art$title
 most.pos<-pos.art %>% filter(sentiment==max(sentiment))
 most.neg<-neg.art %>% filter(sentiment==min(sentiment))
-best.art<-most.pos$url
+best.art<-most.pos$url[1]
 
-worst.art<-most.neg$url
+worst.art<-most.neg$url[1]
 
 
 rD <- rsDriver(browser = 'firefox', phantomver = NULL) 
